ea0cd82646311e6b7050c92eef7727ed81f4c400
Fin <finfreezer@gmail.com>
2025-09-28 15:22:29 +0300

    Static character-analysis complete.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import count_words
 from stats import count_letter_usage
+from stats import sort_by_count
 
 def get_book_text(path_to_file):
     #print("Enter file path:")
@@ -16,9 +17,26 @@ def get_book_text(path_to_file):
     return lines
 
 def main():
-    count_letter_usage( get_book_text("books/frankenstein.txt") )
+    usage_data = { }
+    sorted_data = [ ]
+    location = "books/frankenstein.txt"
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {location}...")
+    
     w = count_words( 
-        get_book_text("books/frankenstein.txt") ) 
+    get_book_text(location) ) 
+   
+    print("----------- Word Count ----------")
     print(f"Found {w} total words")
+    print("--------- Character Count -------")
+    usage_data = count_letter_usage( get_book_text(location) )
+    sorted_data = sort_by_count(usage_data)
+    
+    for letter, num in sorted_data:
+        print(f"{letter["name"]}: {num["num"]}")
+    
+    print("============= END ===============")
+
+
 
 main()
--- stats.py
+++ stats.py
@@ -7,11 +7,32 @@ def count_letter_usage(string):
     recurrence = { }
 
     for char in string:
-        if char.lower() not in found_characters:
+        if char.lower() not in found_characters and char.lower().isalpha():
             found_characters.add( char.lower() )
             recurrence[ char.lower() ] = 1
-        else:
+        elif ( char.lower().isalpha() ):
             recurrence[ char.lower() ] += 1
+        else:
+            continue
 
-    for letter, amount in recurrence.items():
-        print(f" '{letter}': {amount} ")
+    """for letter, amount in recurrence.items():
+        print(f" '{letter}': {amount} ")"""
+    
+    sort_by_count(recurrence)
+    return recurrence
+
+def sort_by_count(dictionary):
+    sorted_list = []
+    for letter, amount in dictionary.items():
+        sorted_list.append(
+            [
+            {"name":letter},
+            {"num":amount}
+            ]
+        )
+
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+def sort_on(items):
+    return items[1]["num"]
